6330c08aca43e087838abd84a56c82de160d3933
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-07-28 03:55:56 +0900

    Title: 빙하, Time: 667ms, Memory: 35MB, Status: Passed - CodeTree

--- 240728/빙하/glacier.py
+++ 240728/빙하/glacier.py
@@ -7,42 +7,40 @@ total = sum(sum(row) for row in arr)  # 전체 빙하의 개수 카운트
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def in_range(x, y):
+def can_go(x, y):
     return 0 <= x < n and 0 <= y < m
 
-def melt():
-    can_be_melt = []
+def melted():
+    new = [[arr[i][j] for j in range(m)] for i in range(n)]
     visited = [[0] * m for _ in range(n)]
 
     q = deque()
     q.append((0, 0))  # 외부에서 시작
+    cnt = 0
     while q:
         x, y = q.popleft()
 
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
 
-            if not in_range(nx, ny):
+            if not can_go(nx, ny):
                 continue
             if visited[nx][ny]:
                 continue
             visited[nx][ny] = 1
             
             if arr[nx][ny] == 1:  # 빙하인 경우
-                can_be_melt.append((nx,ny))  # 녹일 수 있는 빙하 담기
+                new[nx][ny] = 0  # 녹임
+                cnt += 1
             else:
                 q.append((nx, ny))  # 물인 경우 큐에 추가
-    
-    for x,y in can_be_melt: # 동시에 빙하 녹이기
-        arr[x][y] = 0
-
-    return len(can_be_melt)
+    return cnt, new
 
 t = 0
 while total:
-    cnt = melt()
+    cnt, arr = melted()
     t += 1
-    total -= cnt 
-    if total == 0: # 녹일 빙하가 남아있지 않으면 종료
+    total -= cnt
+    if total == 0:
         print(t, cnt)
         break

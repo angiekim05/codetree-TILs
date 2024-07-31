f05a56e629d5523c7750eb5fcc66d697677a0329
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-08-01 02:40:20 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240801/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240801/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,28 +1,17 @@
-import sys
-import copy
+n = int(input())
+arr = [list(map(int,input().split())) for i in range(n)]
+dp = [[[0,10000] for _ in range(n+1)] for _ in range(n+1)]
 
-N = int(sys.stdin.readline())
-field = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-min_field = copy.deepcopy(field)
-max_field = copy.deepcopy(field) 
+def minus(x,y):
+    return [abs(x-y),[x,y]]
 
-for i in range(N):
-    for j in range(N):
-        if i == 0 and j == 0:
-            continue
-        elif i == 0:
-            max_field[i][j] = max((max_field[i][j], max_field[i][j-1]))
-            min_field[i][j] = min((min_field[i][j], min_field[i][j-1]))
-        elif j == 0:
-            max_field[i][j] = max((max_field[i-1][j], max_field[i][j]))
-            min_field[i][j] = min((min_field[i-1][j], min_field[i][j]))
-        else:
-            max_val, min_val = 0, 0
-            max1, min1,= max_field[i-1][j], min_field[i-1][j]
-            max2, min2 = max_field[i][j-1], min_field[i][j-1]
-            max_val = max2 if abs(max1-min1) > abs(max2-min2) else max1
-            min_val = min2 if abs(max1-min1) > abs(max2-min2) else min1
-            max_field[i][j] = max((max_val, max_field[i][j]))
-            min_field[i][j] = min((min_val, min_field[i][j]))
+for i in range(1,1+n):
+    for j in range(1,1+n):
+        dp[i][j] = sorted([minus(*dp[i-1][j]), minus(*dp[i][j-1])])[0][1]
+        dp[i][j][0] = max(dp[i][j][0], arr[i-1][j-1])
+        dp[i][j][1] = min(dp[i][j][1], arr[i-1][j-1])
+        
 
-print(abs(min_field[i][j]-max_field[i][j]))
+
+print(dp[-1][-1][0] - dp[-1][-1][1])
+# print(*dp,sep="\n")

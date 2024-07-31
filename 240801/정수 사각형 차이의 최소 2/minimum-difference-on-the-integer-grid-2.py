8a73e4d0b90b97c4498c1edc7ad26823af86f569
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-08-01 04:01:48 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240801/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,15 +1,23 @@
 n = int(input())
 arr = [list(map(int,input().split())) for i in range(n)]
-dp = [[[0,10000] for _ in range(n+1)] for _ in range(n+1)]
+dp = [[[0,10000] for _ in range(n)] for _ in range(n)]
 
 def minus(x,y):
-    return [abs(x-y),[x,y]]
+    return abs(x-y)
 
-for i in range(1,1+n):
-    for j in range(1,1+n):
-        dp[i][j] = sorted([minus(*dp[i-1][j]), minus(*dp[i][j-1])])[0][1]
-        dp[i][j][0] = max(dp[i][j][0], arr[i-1][j-1])
-        dp[i][j][1] = min(dp[i][j][1], arr[i-1][j-1])
+dp[0][0] = [arr[0][0],arr[0][0]]
+for i in range(1,n):
+    dp[i][0] = [max(arr[i][0], dp[i-1][0][0]),min(arr[i][0], dp[i-1][0][1])]
+    dp[0][i] = [max(arr[0][i], dp[0][i-1][0]),min(arr[0][i], dp[0][i-1][1])]
+
+for i in range(1,n):
+    for j in range(1,n):
+        temp1 = [max(arr[i][j], dp[i-1][j][0]),min(arr[i][j], dp[i-1][j][1])]
+        temp2 = [max(arr[i][j], dp[i][j-1][0]),min(arr[i][j], dp[i][j-1][1])]
+        if minus(*temp1) > minus(*temp2):
+            dp[i][j] = temp2
+        else:
+            dp[i][j] = temp1
         
 
 

3995ed802c210af73f0d22221421b659ab98a06c
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-09-21 16:59:50 +0900

    Title: 함수 실행 횟수, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240921/함수 실행 횟수/number-of-function-executions.py
+++ 240921/함수 실행 횟수/number-of-function-executions.py
@@ -4,9 +4,9 @@ tgt[0] = 1
 tgt[1] = 1
 
 def codetree(n):
-    if tgt[n]:
-        return tgt[n]
-    tgt[n] = codetree(n-1)+codetree(n-2) +1
+    for i in range(2,n+1):
+        tgt[i] = tgt[i-1]+tgt[i-2]+1
+
     return tgt[n]
 
 print(codetree(n)%1000000007)

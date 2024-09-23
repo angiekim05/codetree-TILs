31b104485127aecd166dd2b7abc47626fedc7dfd
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2024-09-24 01:50:04 +0900

    Title: 마법의 숲 탐색, Time: 425ms, Memory: 30MB, Status: Passed - CodeTree

--- 240924/마법의 숲 탐색/magical-forest-exploration.py
+++ 240924/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,5 +1,5 @@
 r,c,k = map(int,input().split())
-forest = [[1]+[0]*c for _ in range(r+2)]
+forest = [[1]+[0]*c for _ in range(r+3)]
 exits = set()
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
@@ -7,7 +7,7 @@ dy = [0,1,0,-1]
 def init_forest():
     global exits
     exits = set()
-    for i in range(r+1):
+    for i in range(1,r+2):
         for j in range(1,c+1):
             forest[i][j] = 0
     return
@@ -53,14 +53,12 @@ def get_row(fx,fy):
 
                 if res == r:
                     return res
-    # print(exits)
-    # print(fx,fy)
+    # print(exits,fx,fy)
     # print(*visited[1:], sep="\n")
-    # print()
     return res
 
 def go_down(idx,y,d):
-    x = 0
+    x = -1
 
     while x < r-1:
         if can_go_south(x,y):
